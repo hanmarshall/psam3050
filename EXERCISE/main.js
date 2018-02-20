9b5ebfd6021371c9b78110b1c83bdfdbf69a58a9
Hannah <marsh984@newschool.edu>
2018-02-20 12:26:02 -0500

    exercise

---
+++ EXERCISE/main.js
@@ -0,0 +1,16 @@
+var didScroll = false;
+
+window.onscroll = doThisStuffOnScroll;
+
+function doThisStuffOnScroll() {
+    didScroll = true;
+    $("span").addClass("punctuation");
+}
+
+setInterval(function() {
+    if(didScroll) {
+        didScroll = false;
+        $("span").removeClass("punctuation");
+    }
+}, 100);
+
